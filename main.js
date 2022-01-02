d41852be662980e3070ced4c0773b19eba91f1d6
Przemek Luty <przemyslawluty92@gmail.com>
2022-01-02 22:44:15 +0100

    normalize.css

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-const firstName = 'Przemek';
+const firstName = "Przemek";
 const age = 30;
-console.log (firstName);
-console.log (age);
-console.log(`Siema, nazywam się ${firstName} i mam ${age} lat.`)
+console.log(firstName);
+console.log(age);
+console.log(`Siema, nazywam się ${firstName} i mam ${age} lat.`);
+
+
+const heading = document.querySelector('.main-heading--js');
+
+console.log(heading.innerHTML)
